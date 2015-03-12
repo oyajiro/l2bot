05d1b4d22709fcbaa785a715905ba8a3abf138f7
kouta <kouta@list.ru>
2015-03-13 01:01:19 +0300

    Fix hp issue.

--- ocr.py
+++ ocr.py
@@ -20,6 +20,9 @@ def getScreen(x1, y1, x2, y2):
     screen = ImageGrab.grab(box)
     img =  array(screen.getdata(),dtype=uint8).reshape((screen.size[1],screen.size[0],3))
     # img = cv2.imread('snap__1426174983.png')
+    # cv2.imshow('image',img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     return img
 
 def findTarget(img):    
@@ -45,13 +48,11 @@ def findTarget(img):
         center = round((right[0]+left[0])/2)
         center = int(center)
         moveMouse(center-10,left[1]+70)
-        time.sleep(1)
         if (findFromTargeted(template_tg, left, right)):
             autoit.mouse_click('left', center-10, left[1]+70)
             return True
         pyautogui.moveTo(center,left[1]+70)
         moveMouse(center,left[1]+70)
-        time.sleep(1)
         if (findFromTargeted(template_tg, left, right)):
             autoit.mouse_click('left', center+10, left[1]+70)
             return True
@@ -68,7 +69,7 @@ def findFromTargeted(template, left, right):
     ret,tp2 = cv2.threshold(tp1,100,255,cv2.THRESH_BINARY)
     wth, hth = cv.GetSize(th2)
     wtp, htp = cv.GetSize(tp2)
-    if (wth > wtp && hth > htp):
+    if wth > wtp and hth > htp:
         res = cv2.matchTemplate(th2, tp2, cv2.TM_CCORR_NORMED)
         if (res.any()):
             # cv2.imwrite('th2' + str(int(time.time())) + '.png',th2)
@@ -82,23 +83,29 @@ def findFromTargeted(template, left, right):
     return False
 def grabHP():
     hp = getScreen(leftCornerx + 958,leftCornery + 16,leftCornerx + 1111,leftCornery + 25)
-    gray = cv2.cvtColor(hp, cv2.COLOR_BGR2GRAY)
 
     # img = cv2.imread('snap__1426174990.png')
     # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # roi = gray[16:25, 958:1111];
-    return gray
+    return hp
 
 
 def findHP(img):
-    statuses = { 'dead' : 0,  'lhalf' : 1, 'mhalf' : 2, 'full' : 3}
-    gray = grabHP()
-    # roi = gray[leftCornery + 16:leftCornery + 25, leftCornerx + 958:leftCornerx + 1111];
-    ret,th1 = cv2.threshold(gray,100,255,cv2.THRESH_BINARY)
-    cv2.imwrite('hpgrab' + str(int(time.time())) + '.png',th1)
+    statuses = {'none': -1, 'dead' : 0,  'lhalf' : 1, 'mhalf' : 2, 'full' : 3}
+    hpcolor = [132, 73, 231]
+    hp = grabHP()
+    gray = cv2.cvtColor(hp, cv2.COLOR_BGR2GRAY)
+    ret,th1 = cv2.threshold(gray,130,255,cv2.THRESH_TOZERO_INV)
+    ret,th1 = cv2.threshold(th1,120,255,cv2.THRESH_TOZERO)
+
     (cnts, hierarchy) = cv2.findContours(th1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     if (len(cnts) == 0):
         return statuses['dead']
+    left = list(cnts[0][cnts[0][:,:,0].argmin()][0])
+    px = hp[left[1], left[0]];
+    if (hpcolor != px).any():
+        return statuses['none']
+
     leftx = list(cnts[0][cnts[0][:,:,0].argmin()][0])[0]
     rightx = list(cnts[0][cnts[0][:,:,0].argmax()][0])[0]
     diff = rightx - leftx
@@ -115,6 +122,7 @@ def moveMouse(x,y):
 
 def main():
     title = "[CLASS:l2UnrealWWindowsViewportWindow]"
+    autoit.win_wait(title, 5)
     counter = 0
     while True:
         img = getScreen(leftCornerx,leftCornery,x2,y2)
