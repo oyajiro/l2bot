415d81a91ef05597087dcef4c1a67e86e5135e8c
kouta <kouta@list.ru>
2015-09-28 19:39:39 +0300

    Many changes.

--- hf/Bot.py
+++ hf/Bot.py
@@ -11,30 +11,41 @@ class Bot:
     leftCornerx = 7
     leftCornery = 38
     x2 = 1661
-    y2 = 360
-    title = "[CLASS:l2UnrealWWindowsViewportWindow]"
+    y2 = 870
+    title = "[TITLE:Lineage II]"
 
     def getScreen(sefl, x1, y1, x2, y2):
         box = (x1, y1, x2, y2)
         screen = ImageGrab.grab(box)
         img =  array(screen.getdata(),dtype=uint8).reshape((screen.size[1],screen.size[0],3))
-        img = cv2.imread('test2.png')
-        img = img[y1:y2, x1:x2]
+        # img = cv2.imread('test2.png')
+        # img = img[y1:y2, x1:x2]
         return img
 
     def findTarget(self):
         img = self.getScreen(self.leftCornerx,self.leftCornery,self.x2,self.y2)
-        targetColor = [154, 228, 166]
-        arr = array(targetColor, dtype='uint8')
-        mask = cv2.inRange(img, arr, arr)
+        cv2.rectangle(img, (582, 407), (1100, 750), [0, 0, 0], -1)
+        cv2.rectangle(img, (0, 0), (520, 77), [0, 0, 0], -1)
+        # cv2.imshow("images", img)
+        # cv2.waitKey(0)
+        # bla()
+        colorMin = [255, 131, 86]
+        colorMax = [255, 167, 108]
+        mask = cv2.inRange(img, array(colorMin, dtype='uint8'), array(colorMax, dtype='uint8'))
+        mask = cv2.filter2D(mask,-1, ones((5,5), float32))
+        (cnts, hierarchy) = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        colorMin = [166, 228, 154]
+        colorMax = [169, 232, 156]
+        mask = cv2.inRange(img, array(colorMin, dtype='uint8'), array(colorMax, dtype='uint8'))
         mask = cv2.filter2D(mask,-1, ones((15,15), float32))
         (cnts, hierarchy) = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-                
+        cnts = sorted(cnts, key=lambda cnt: self.contourHeight(cnt), reverse=True);        
         # cv2.drawContours(img, cnts, -1, (255,0,0), 3)
         # cv2.imshow("images", img)
         # cv2.waitKey(0)
         # bla()
         for cnt in cnts:
+            x,y,w,h = cv2.boundingRect(cnt)
             left = list(cnt[cnt[:,:,0].argmin()][0])        
             right = list(cnt[cnt[:,:,0].argmax()][0])
             print 'left x' + str(left[0])+ 'y '+ str(left[1])
@@ -42,26 +53,34 @@ class Bot:
             if right[0] - left[0] < 20:
                 print 'Small diff ' + str(right[0] - left[0])
                 continue
-            center = round((right[0]+left[0])/2)
+            x,y,w,h = cv2.boundingRect(cnt)
+            test = self.getScreen(x,y+h,x+w,y+200)
+            cv2.imwrite('tgt/' + str(int(time.time())) + '_not_chmp.png',test)
+
+            center = round((right[0]+left[0]+3)/2)
             center = int(center)
             self.moveMouse(center,left[1]+90)
-            self.sleep(0.2,0.4)
-            res = self.findHP(img);
+            self.sleep(0.3,0.5)
+            autoit.control_send(self.title, '', '{F3}', 0)
+            res = self.findHP();
             if res > 0:
                 autoit.control_send(self.title, '', '{F1}', 0)
                 self.sleep(0.1,0.4)
                 return
 
-            if (findFromTargeted(left, right)):
+            if (self.findFromTargeted(left, right)):
                 autoit.mouse_click('left', center, left[1]+90)
                 self.sleep(0.1,0.3)
                 autoit.mouse_click('left', center, left[1]+90)
                 return True
-        mouseRotate()
+        self.mouseRotate()
 
-    def findFromTargeted(left, right):
-        template = cv2.imread('template_target2.png', 0)
-        roi = getScreen(left[0]-70+self.leftCornerx, left[1]-15+self.leftCornery, right[0]+70+self.leftCornerx, right[1]+12+self.leftCornery)
+    def findFromTargeted(self, left, right):
+        template = cv2.imread('target_template.png', 0)
+        roi = self.getScreen(left[0]-70+self.leftCornerx, left[1]+self.leftCornery-10, right[0]+70+self.leftCornerx, right[1]+30+self.leftCornery)
+        # cv2.imshow("images", roi)
+        # cv2.waitKey(0)
+        # bla()
         roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
         ret,th1 = cv2.threshold(roi,224,255,cv2.THRESH_TOZERO_INV)
         ret,th2 = cv2.threshold(th1,135,255,cv2.THRESH_BINARY)
@@ -79,21 +98,42 @@ class Bot:
                 if (max_val > 0.7):
                     return True
                 else:
+                    cv2.imwrite('tgt/' + str(int(time.time())) + '_roitgt.png',roi)
                     return False
         return False
 
 
-    def matchBuff(buffname, x, y):
-        template = cv2.imread(buffname, 0)
-        roi = getScreen(x, y, x+32, y+32)
+    def matchBuff(self, buffname):
+        template = cv2.imread('buffs/' + buffname, 0)
+        roi = self.getScreen(self.leftCornerx, self.leftCornery - 5, 600, 140)
         roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
         res = cv2.matchTemplate(roi, template, cv2.TM_CCORR_NORMED)
         if (res.any()):
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-            print 'buffname %.2f'%max_val
+            print buffname + '%.2f'%max_val
             if (max_val > 0.98):
                 return True
-        print 'not any'
+        print 'not_buff '+ buffname
+        return False
+
+    def clickBuff(self, clickname):
+        template = cv2.imread('buffs/' + clickname, 0)
+        th, tw = template.shape[:2]
+        roi = self.getScreen(352, 875, 865, 1063)
+        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        res = cv2.matchTemplate(roi, template, cv2.TM_CCORR_NORMED)
+        if (res.any()):
+            min_val, max_val, min_loc, (x1,y1) = cv2.minMaxLoc(res)
+            # roi2 = roi[y1:y1+th, x1:x1+tw]
+            # print 352+x1
+            # print 875+y1
+            # cv2.imshow("2", roi)
+            # cv2.waitKey(0)
+            # bla()
+            if (max_val > 0.98):
+                autoit.mouse_click('left', 352+x1+int(round(tw/2)), 875+y1+int(round(th/2)))
+                return True
+        print 'not_click '+ clickname
         return False
 
     def grabHP(self):
@@ -102,10 +142,10 @@ class Bot:
         return hp
 
 
-    def findHP(self, img):
+    def findHP(self):
         statuses = {'none': -1, 'dead' : 0,  'lhalf' : 1, 'mhalf' : 2, 'full' : 3}
-        hpcolorMin = [18, 23, 111]
-        hpcolorMax = [21, 23, 111]
+        hpcolorMin = [111, 23, 18]
+        hpcolorMax = [111, 23, 21]
         deadcolor = [58, 57, 68]
         hp = self.grabHP()
 
@@ -129,7 +169,7 @@ class Bot:
         leftx = list(cnts[0][cnts[0][:,:,0].argmin()][0])[0]
         rightx = list(cnts[0][cnts[0][:,:,0].argmax()][0])[0]
         diff = rightx - leftx
-        if diff > 140:
+        if diff > 147:
             return statuses['full']
         if diff >= 75:
             return statuses['mhalf']
@@ -144,9 +184,12 @@ class Bot:
         time.sleep(random.uniform(a, b))
         return True
 
-    def mouseRotate():
+    def randClick(self, a, b, button):
+        autoit.mouse_click(button, int(random.uniform(a+2, a-2)), int(random.uniform(b+2, b-2)))
+
+    def mouseRotate(self):
         autoit.mouse_move(655, 521)
-        sleep(0.2, 0.6)
+        self.sleep(0.2, 0.6)
         autoit.mouse_down('right')
         autoit.mouse_move(670, 521)
         autoit.mouse_up('right')
@@ -174,29 +217,30 @@ class Bot:
         if diff < 90:
             return statuses['lhalf']
 
-    def checkOwnHp():
+    def checkOwnHp(self):
         statuses = {'verylow' : -1, 'dead' : 0,  'lhalf' : 1, 'mhalf' : 2, 'full': 3}
-        hpcolor = [80, 87, 130]
-        hpdeadcolor = [57,44,41]
-        hpCord = (24, 71, 175, 84)
+        hpcolor = [130, 87, 80]
+        hpdeadcolor = [68,54,48]
+        hpCord = (34 - self.leftCornerx, 104 - self.leftCornery, 190 - self.leftCornerx, 109 - self.leftCornery)
         hp = ImageGrab.grab(hpCord)
         imgHP =  array(hp.getdata(),dtype=uint8).reshape((hp.size[1],hp.size[0],3))
         # hparr = array(hpcolor, dtype='uint8')
         # mask = cv2.inRange(hp, hparr, hparr)
         # mask = cv2.filter2D(mask,-1, ones((2,17), float32))
-        px = imgHP[4, 28];
+        px = imgHP[2, 28];
         if (px == hpdeadcolor).all():
             return statuses['verylow']
-        px = imgHP[4, 5];
-        if (px == hpdeadcolor).all():
-            winsound.PlaySound("SystemHand", winsound.SND_ALIAS)
-            return statuses['dead']
         hparr = array(hpcolor, dtype='uint8')
         mask = cv2.inRange(imgHP, hparr, hparr)
+        mask = cv2.filter2D(mask,-1, ones((2,17), float32))
         (cnts, hierarchy) = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
+        # cv2.drawContours(imgHP, cnts, -1, (255,0,0), 3)
+        # cv2.imshow("images", imgHP)
+        # cv2.waitKey(0)
+        # bla()
         if (len(cnts) == 0):
-            return statuses['lhalf'];
+            winsound.PlaySound("SystemHand", winsound.SND_ALIAS)
+            return statuses['dead']
 
         left = list(cnts[0][cnts[0][:,:,0].argmin()][0])
         right = list(cnts[0][cnts[0][:,:,0].argmax()][0])
@@ -206,16 +250,20 @@ class Bot:
         if diff < 90:
             return statuses['lhalf']
 
-    def checkOwnMp():
+    def checkOwnMp(self):
         statuses = {'none': 0, 'less': 1,  'lhalf': 2, 'mhalf': 3}
-        mpcolor = [82,109,148]
-        mpCord = (24, 83, 175, 97)
+        mpcolor = [76,98,136]
+        mpCord = (26, 80, 181, 85)
         mp = ImageGrab.grab(mpCord)
         imgmp =  array(mp.getdata(),dtype=uint8).reshape((mp.size[1],mp.size[0],3))
         mparr = array(mpcolor, dtype='uint8')
         mask = cv2.inRange(imgmp, mparr, mparr)
+        mask = cv2.filter2D(mask,-1, ones((2,17), float32))
         (cnts, hierarchy) = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
+        # cv2.drawContours(imgmp, cnts, -1, (255,0,0), 3)
+        # cv2.imshow("images", imgmp)
+        # cv2.waitKey(0)
+        # bla()
         if (len(cnts) == 0):
             return statuses['none']
 
@@ -256,3 +304,62 @@ class Bot:
                 autoit.mouse_click('left', 1043, 92)
                 sleep(0.4,0.9)
                 autoit.mouse_click('left', 1378, 670)
+
+    def contourHeight(self, cnt):
+        x,y,w,h = cv2.boundingRect(cnt)
+        img = self.getScreen(x,y+h,x+w,y+200)
+        colorMin = [255, 131, 86]
+        colorMax = [255, 167, 108]
+        mask = cv2.inRange(img, array(colorMin, dtype='uint8'), array(colorMax, dtype='uint8'))
+        mask = cv2.filter2D(mask,-1, ones((5,5), float32))
+        (cnts, hierarchy) = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        if (not cnts):
+            return y
+        else:
+            print "champ!!!"
+            cv2.imwrite('tgt/' + str(int(time.time())) + '_chmp.png',img)
+            return y*100
+
+    def checkPetHp(self):
+        statuses = {'verylow' : -1, 'dead' : 0,  'lhalf' : 1, 'mhalf' : 2, 'full': 3}
+        hpcolor = [127, 70, 68]
+        hpCord = (355, 748, 535, 800)
+        hp = ImageGrab.grab(hpCord)
+        imgHP =  array(hp.getdata(),dtype=uint8).reshape((hp.size[1],hp.size[0],3))
+        if (px == hpdeadcolor).all():
+            winsound.PlaySound("SystemHand", winsound.SND_ALIAS)
+            return statuses['dead']
+        hparr = array(hpcolor, dtype='uint8')
+        mask = cv2.inRange(imgHP, hparr, hparr)
+        mask = cv2.filter2D(mask,-1, ones((2,17), float32))
+        (cnts, hierarchy) = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        # cv2.drawContours(imgHP, cnts, -1, (255,0,0), 3)
+        # cv2.imshow("images", imgHP)
+        # cv2.waitKey(0)
+        # bla()
+        if (len(cnts) == 0):
+            return statuses['dead'];
+
+        left = list(cnts[0][cnts[0][:,:,0].argmin()][0])
+        right = list(cnts[0][cnts[0][:,:,0].argmax()][0])
+        diff = right[0] - left[0]
+        if diff >= 80:
+            return statuses['mhalf']
+        if diff < 80:
+            return statuses['lhalf']
+
+    def checkSystemChatColor(self, colorMin, colorMax):
+        systemChatCoord = (27, 751, 350, 870)
+        grab = ImageGrab.grab(systemChatCoord)
+        img =  array(grab.getdata(),dtype=uint8).reshape((grab.size[1],grab.size[0],3))
+        mask = cv2.inRange(img, array(colorMin, dtype='uint8'), array(colorMax, dtype='uint8'))
+        mask = cv2.filter2D(mask,-1, ones((2,30), float32))
+        (cnts, hierarchy) = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        # cv2.drawContours(img, cnts, -1, (255,0,0), 3)
+        # cv2.imshow("images", img)
+        # cv2.waitKey(0)
+        # bla()
+        if (len(cnts) > 0):
+            return 1
+
+        return 0
--- hf/dagger.py
+++ hf/dagger.py
@@ -8,55 +8,63 @@ import random
 from Bot import  *
 
 def main():
-    # autoit.win_wait(title, 5)
     bot = Bot()
-    bot.findTarget()
-    # counter = 0
-    # poitonUse = 0
-    # cycle = True
-    # while cycle:
-    #     hpstatus = checkOwnHp()
-    #     print 'hp ' + str(hpstatus)
-    #     if hpstatus == -1:
-    #         print 'CPDamage'
-    #         cv2.imwrite('CPDamage' + str(int(time.time())) + '.png',getScreen(leftCornerx,leftCornery,x2,fullY2))
-    #         cycle = False
-    #     if hpstatus == 0:
-    #         print 'Dead'
-    #         cv2.imwrite('Dead' + str(int(time.time())) + '.png',getScreen(leftCornerx,leftCornery,x2,fullY2))
-    #         cycle = False
-    #     if hpstatus == 1:
-    #         if poitonUse == 0:
-    #             autoit.control_send(title, '', '{F10}', 0)
-    #         poitonUse += 1
-    #         if poitonUse > 5:
-    #             poitonUse = 0
-    #     else:
-    #         poitonUse = 0
-    #     img = getScreen(leftCornerx,leftCornery,x2,y2)
-    #     res = findHP(img);
-    #     print 'tgs ' + str(res)
+    autoit.win_wait(bot.title, 5)
+    counter = 0
+    poitonUse = 0
+    cycle = True
+    fullCounter = 0
+    while cycle:
+        hpstatus = bot.checkOwnHp()
+        print 'hp ' + str(hpstatus)
+        if hpstatus == 0:
+            print 'Dead'
+            cv2.imwrite('Dead' + str(int(time.time())) + '.png',bot.getScreen(leftCornerx,leftCornery,x2,fullY2))
+            cycle = False
+        if hpstatus == 1:
+            if poitonUse == 0:
+                autoit.control_send(bot.title, '', '{F10}', 0)
+            poitonUse += 1
+            if poitonUse > 5:
+                poitonUse = 0
+        else:
+            poitonUse = 0
+        res = bot.findHP();
+        print 'tgs ' + str(res)
+        if res == 3:
+            fullCounter += 1
+            print 'fc ' + str(fullCounter)
+        else:
+            fullCounter = 0
+        if fullCounter > 6:
+            autoit.control_send(bot.title, '', '{ESC}', 0)
+            bot.sleep(0.3,0.6)
+            autoit.control_send(bot.title, '', '{F3}', 0)
+            bot.mouseRotate()
+            fullCounter = 0
+            
+        if res > 0:
+            autoit.control_send(bot.title, '', '{F1}', 0)
+            counter = 0
 
-    #     if res > 0:
-    #         autoit.control_send(title, '', '{F1}', 0)
-    #         counter = 0
+            if res == 1 or res == 3:
+                bot.sleep(0.3,0.6)
+            if res > 1 and res < 3:
+                bot.sleep(1,3)
+        else:
+            fullCounter = 0
+            if counter < 2:
+                autoit.control_send(bot.title, '', '{F3}', 0)
+                bot.sleep(0.1,0.2)
+                autoit.control_send(bot.title, '', '{F1}', 0)
 
-    #         if res == 1:
-    #             sleep(0.3,0.6)
-    #         if res > 1:
-    #             sleep(2,4)
-    #     else:
-    #         if counter < 2:
-    #             autoit.control_send(title, '', '{F3}', 0)
-    #             sleep(0.1,0.2)
-    #             autoit.control_send(title, '', '{F1}', 0)
-
-    #         if counter > 1:
-    #             Bot.findTarget()
-    #             counter = 0
-    #         counter += 1
-    #     print 'cnt ' + str(counter)
-    # pass
+            if counter > 1:
+                bot.findTarget()
+                # autoit.control_send(bot.title, '', '{F7}', 0)
+                counter = 0
+            counter += 1
+        print 'cnt ' + str(counter)
+    pass
 
 if __name__ == '__main__':
     main()
